80b34106c8d0918e797f9af53988f36010c19f20
jsy0404 <pnmsegwyd@naver.com>
2023-08-12 13:23:40 +0900

    feat: 사용자 잔고 증감 구현

---
+++ hackathon1/src/main/java/com/koscom/hackathon1/controller/BalanceController.java
@@ -0,0 +1,38 @@
+package com.koscom.hackathon1.controller;
+
+import com.koscom.hackathon1.request.BalanceAddRequest;
+import com.koscom.hackathon1.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+    private final UserService userService;
+
+    BalanceController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getBalance(@PathVariable String userId) {
+        int balance = userService.getUser(userId).getBalance();
+
+        return ResponseEntity.ok(balance);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> add(@RequestBody BalanceAddRequest request) {
+        userService.addBalance(request.getUserId(), request.getAmount());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/minus")
+    public ResponseEntity<Object> minus(@RequestBody BalanceAddRequest request) {
+        userService.minusBalance(request.getUserId(), request.getAmount());
+
+        return ResponseEntity.ok().build();
+    }
+}
